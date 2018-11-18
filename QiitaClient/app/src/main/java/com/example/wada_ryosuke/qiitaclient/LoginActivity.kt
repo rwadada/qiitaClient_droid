40392f25a6792e38f3714a6b22307d07d5aab17d
rwadada <wadada.knct@gmail.com>
2018-11-18 23:31:59 +0900

    [ticket: #2] [update] update UI
    description
    UIにログインボタンとゲストボタンを追加、背景にそれっぽいものを設定

--- QiitaClient/app/src/main/java/com/example/wada_ryosuke/qiitaclient/LoginActivity.kt
+++ QiitaClient/app/src/main/java/com/example/wada_ryosuke/qiitaclient/LoginActivity.kt
@@ -1,12 +1,20 @@
 package com.example.wada_ryosuke.qiitaclient
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.wada_ryosuke.qiitaclient.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        var mActivityLoginBinding: ActivityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        val titl: TextView = mActivityLoginBinding.titleTextview
+        titl.setText("  Qiita\nCLIENT")
+        mActivityLoginBinding.guestButton.setText("Guest")
+        mActivityLoginBinding.loginButton.setText("Login")
     }
 }
